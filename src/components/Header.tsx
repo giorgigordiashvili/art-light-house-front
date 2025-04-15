6725b68906e20abbd635318a6d9975d410f600bf
GNatroshvili <gigi.natroshvili@gmail.com>
2025-04-15 21:41:55 +0400

    Add bottom-border to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,10 +15,10 @@ const StyledContainer = styled.div`
   padding: 20px 20px;
   background-color: rgba(11, 11, 11, 0.34);
   backdrop-filter: blur(98.80000305175781px);
+  border-bottom: 1px solid #FFFFFF14;
   z-index: 1001;
-
   @media (max-width: 1080px) {
-    padding: 17px 0;
+    padding: 20px 0;
   }
 `;
 
