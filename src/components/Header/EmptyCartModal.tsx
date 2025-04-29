335f0ceee1d4f532b7de52b7d9be593a5c503a01
GNatroshvili <gigi.natroshvili@gmail.com>
2025-04-29 19:44:42 +0400

    Fix UserMenu and EmptyCartModal positions

--- src/components/Header/EmptyCartModal.tsx
+++ src/components/Header/EmptyCartModal.tsx
@@ -55,15 +55,18 @@ const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: absolute;
+  position: fixed;
   top: 99px;
-  right: 0;
   background-color: #1a1a1a96;
   backdrop-filter: blur(114px);
   border: 1px solid #ffffff12;
   border-radius: 17px;
   padding: 0;
   z-index: 1001;
+
+  @media (max-width: 1332px) {
+    right: 20px;
+  }
 
   @media (max-width: 1080px) {
     position: fixed;
@@ -86,15 +89,30 @@ const StyledImage = styled.div`
   }
 `;
 
+const ModalLayoutWrapper = styled.div`
+  width: 100%;
+  margin: auto;
+`;
+
+const ModalLayout = styled.div`
+  width: 1292px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
 const EmptyCartModal = () => {
   return (
-    <StyledContainer>
-      <StyledImage>
-        <Image src="/assets/EmptyCartImage.svg" width={123} height={123} alt="cart-icon" />
-      </StyledImage>
-      <CartTitle text="კალათა ცარიელია" />
-      <CartDescription text="დაამატე პროდუქტები ქალათში და გააფორმე შეკვეთა" />
-    </StyledContainer>
+    <ModalLayoutWrapper>
+      <ModalLayout>
+        <StyledContainer>
+          <StyledImage>
+            <Image src="/assets/EmptyCartImage.svg" width={123} height={123} alt="cart-icon" />
+          </StyledImage>
+          <CartTitle text="კალათა ცარიელია" />
+          <CartDescription text="დაამატე პროდუქტები ქალათში და გააფორმე შეკვეთა" />
+        </StyledContainer>
+      </ModalLayout>
+    </ModalLayoutWrapper>
   );
 };
 
