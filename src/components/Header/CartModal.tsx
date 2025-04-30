5293d87009f79b1777efb134737274ced1d742be
GNatroshvili <gigi.natroshvili@gmail.com>
2025-04-30 12:56:27 +0400

    Starting Cart modal

---
+++ src/components/Header/CartModal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div``;
+
+type Props = {};
+
+const CartModal = (props: Props) => {
+  return <StyledContainer></StyledContainer>;
+};
+
+export default CartModal;
