012f23604c4d4819c70d8cafb82a76f7a3f47a8d
GNatroshvili <gigi.natroshvili@gmail.com>
2025-05-13 14:58:16 +0400

    Delete unused lines

--- src/components/Address/EditIcon.tsx
+++ src/components/Address/EditIcon.tsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// import styled from "styled-components";
-// import Image from "next/image";
-
-// const StyledContainer = styled.div`
-//   padding: 9px;
-//   border-radius: 50%;
-//   border: 1px solid #ffffff1a;
-//   width: 40px;
-//   height: 40px;
-// `;
-// type Props = {};
-
-// const EditIcon = (props: Props) => {
-//   return (
-//     <StyledContainer>
-//       <Image src={"/assets/Edit.svg"} width={24} height={24} alt="add-icon" />
-//     </StyledContainer>
-//   );
-// };
-
-// export default EditIcon;
-
 import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
