3cb12692ea6a1a300e5eb665663b57c9040d7d9a
GNatroshvili <gigi.natroshvili@gmail.com>
2025-05-15 21:42:41 +0400

    Add lights to Contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,10 +4,8 @@ import styled from "styled-components";
 import ContactTitle from "./ContactTitle";
 import Container from "../ui/Container";
 import SectionTitle from "../MainPage/SectionTitle";
-import Circle from "../ui/Circle";
 
 const StyledContainer = styled.div`
-  position: relative;
   padding-inline: 20px;
   @media (max-width: 1080px) {
     padding-inline: 0;
@@ -15,15 +13,64 @@ const StyledContainer = styled.div`
 `;
 
 const StyledCircle = styled.div`
+  width: 284px;
+  height: 284px;
+  opacity: 0.64;
   position: absolute;
-  left: 37%;
+  left: 50%;
   transform: translateX(-50%);
-  bottom: 120px;
+  background: radial-gradient(
+    circle,
+    rgba(255, 203, 64, 0.44) 0%,
+    rgba(255, 203, 64, 0.1) 80%,
+    rgba(255, 203, 64, 0) 100%
+  );
+  filter: blur(100px);
+  border-radius: 50%;
+  z-index: 1;
   @media (max-width: 1080px) {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    bottom: -50px;
+    display: none;
+  }
+`;
+
+const StyledLeftCircle = styled.div`
+  width: 284px;
+  height: 284px;
+  opacity: 0.64;
+  position: absolute;
+  bottom: 35px;
+  background: radial-gradient(
+    circle,
+    rgba(255, 203, 64, 0.44) 0%,
+    rgba(255, 203, 64, 0.1) 80%,
+    rgba(255, 203, 64, 0) 100%
+  );
+  filter: blur(100px);
+  border-radius: 50%;
+  z-index: 1;
+  @media (max-width: 1080px) {
+    display: none;
+  }
+`;
+
+const StyledRightCircle = styled.div`
+  width: 284px;
+  height: 284px;
+  opacity: 0.64;
+  position: absolute;
+  bottom: -70px;
+  right: 0;
+  background: radial-gradient(
+    circle,
+    rgba(255, 203, 64, 0.44) 0%,
+    rgba(255, 203, 64, 0.1) 80%,
+    rgba(255, 203, 64, 0) 100%
+  );
+  filter: blur(100px);
+  border-radius: 50%;
+  z-index: 1;
+  @media (max-width: 1080px) {
+    display: none;
   }
 `;
 
@@ -57,9 +104,9 @@ type ContactProps = {
 const Contact: React.FC<ContactProps> = ({ variant = "1" }) => {
   return (
     <StyledContainer>
-      <StyledCircle>
-        <Circle size="large" />
-      </StyledCircle>
+      <StyledCircle />
+      <StyledLeftCircle />
+      <StyledRightCircle />
       <Container>
         {variant === "1" && <ContactTitle text="კონტაქტი" />}
         {variant === "2" && (
