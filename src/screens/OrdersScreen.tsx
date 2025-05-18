67e76db3ba7341aaf6f68f73bcb50d5858ba19d3
GNatroshvili <gigi.natroshvili@gmail.com>
2025-05-18 16:44:32 +0400

    Add lights to orders

--- src/screens/OrdersScreen.tsx
+++ src/screens/OrdersScreen.tsx
@@ -1,6 +1,9 @@
 "use client";
 import styled from "styled-components";
 import OrderMain from "@/MyOrders/OrderMain";
+import Circle from "@/components/ui/Circle";
+import NewCircle from "@/components/ui/NewCircle";
+import LeftCircle from "@/components/ui/LeftCircle";
 
 const StyledComponent = styled.div`
   background: black;
@@ -11,9 +14,33 @@ const StyledComponent = styled.div`
   align-items: center;
 `;
 
+const StyledCircle = styled.div`
+  position: absolute;
+  left: 43%;
+  top: 170px;
+  transform: translateX(-50%);
+  @media (max-width: 1080px) {
+    top: 240px;
+  }
+  @media (max-width: 505px) {
+    left: 30%;
+  }
+  @media (max-width: 409px) {
+    left: 20%;
+  }
+  @media (max-width: 355px) {
+    left: 10%;
+  }
+`;
+
 const OrdersScreen = () => {
   return (
     <StyledComponent>
+      <StyledCircle>
+        <Circle size="small" />
+      </StyledCircle>
+      <LeftCircle size="small" left="-140px" top="900px" media="yes" />
+      <NewCircle size="small" right="142px" top="1000px" media="yes" />
       <OrderMain></OrderMain>
     </StyledComponent>
   );
