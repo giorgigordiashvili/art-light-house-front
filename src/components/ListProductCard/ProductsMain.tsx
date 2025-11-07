a050c4b9928043c240d393746cee1c32c2dd8371
GNatroshvili <gigi.natroshvili@gmail.com>
2025-11-07 16:59:55 +0400

    Fix fetched products count moving bug

--- src/components/ListProductCard/ProductsMain.tsx
+++ src/components/ListProductCard/ProductsMain.tsx
@@ -46,13 +46,18 @@ const ResultsTitle = styled.h2`
   line-height: 24px;
   color: #ffffff90;
   margin-top: -10px;
-  margin-bottom: 32px;
 
   @media (max-width: 1080px) {
     font-size: 16px;
     margin-top: -10px;
-    margin-bottom: 32px;
   }
+`;
+
+const EmptyResultsTitle = styled.h2`
+  position: relative;
+  margin-top: -10px;
+  height: 24px;
+  width: 100%;
 `;
 
 const ContentWrapper = styled.div`
@@ -67,6 +72,7 @@ const SortWrapper = styled.div`
   justify-content: flex-end;
   align-items: center;
   margin-bottom: 18px;
+  padding-top: 10px;
   @media (max-width: 1080px) {
     justify-content: space-between;
   }
@@ -331,7 +337,11 @@ function ProductsMain({ dictionary }: any) {
     <StyledComponent>
       <Container>
         <PageTitle>{dictionary.title}</PageTitle>
-        {resultsTitleMessage && <ResultsTitle>{resultsTitleMessage}</ResultsTitle>}
+        {resultsTitleMessage ? (
+          <ResultsTitle>{resultsTitleMessage}</ResultsTitle>
+        ) : (
+          <EmptyResultsTitle />
+        )}
         <SortWrapper>
           <OnMobile>
             <FilterButton onClick={toggleMobileFilterDropdown} dictionary={dictionary} />
